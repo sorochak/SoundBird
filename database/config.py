3e816aa31233f57749ba6a25438dcaeb42167feb
Austen Sorochak <austensorochak@gmail.com>
2025-07-28 12:06:28 -0700

    update declarative_base import in database/config.py

--- database/config.py
+++ database/config.py
@@ -1,7 +1,7 @@
 # backend/app/database.py
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
